e3cdf00278836aabf58f56a10515e37d9de81196
Boris Paillard <boris.paillard@gmail.com>
2014-01-02 10:28:30 +0100

    more explicit debug message on self

--- 02. Stupid-coaching/app/controllers/welcome.rb
+++ 02. Stupid-coaching/app/controllers/welcome.rb
@@ -1,12 +1,13 @@
 get '/' do
   @coach_message = params[:coach_message] || "what do you want kid ?"
   # Look in app/views/index.erb
-  puts "Context's instance variable from the GET '/' route : #{self.instance_variables}"
+  puts "self class: #{self.class}"
+  puts "self instance variables from GET '/' route handler: #{self.instance_variables}"
   erb :index
 end
 
 post '/coach' do
-  puts "Context's instance variable from the POST '/coach' route : #{self.instance_variables}"
+  puts "self instance variable from POST '/coach' route handler: #{self.instance_variables}"
   
   "Implement your coach response ! params => #{params.inspect}"
 end
