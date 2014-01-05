923d3897ac1f93d3fbfa150dd0e35cb858c755c5
Boris Paillard <boris.paillard@gmail.com>
2014-01-05 14:47:44 +0100

    change debug message stupid coaching

--- 02. Stupid-coaching/app/controllers/welcome.rb
+++ 02. Stupid-coaching/app/controllers/welcome.rb
@@ -1,6 +1,8 @@
 get '/' do
   @coach_message = params[:coach_message] || "what do you want kid ?"
   # Look in app/views/index.erb
+  
+  puts "self : #{self}"
   puts "self class: #{self.class}"
   puts "self instance variables from GET '/' route handler: #{self.instance_variables}"
   erb :index
