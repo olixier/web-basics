596eb2a6d9ea92f968129b66906e73521f49d2dd
Olivier Xu <olivier.xu@gmail.com>
2014-02-05 17:00:30 +0100

    first attempt at chiffres et lettres

--- 03. Numbers-and-letters/app/controllers/welcome.rb
+++ 03. Numbers-and-letters/app/controllers/welcome.rb
@@ -1,7 +1,31 @@
 get '/' do
+	@game = Game.new
+	@game.time_start = Time.now
+	session[:new_entry] = @game
   erb :index
 end
 
 post '/guess' do
+	@last_game = session[:new_entry]
+	@last_game.answer = params[:guess]
+
+
+	if @last_game.test_answer?
+		translation = @last_game.translate_answer
+		points = @last_game.points
+		time = Time.now - @last_game.time_start
+		@message = "Your word means #{translation}, you earned #{points} points in #{time} seconds!"
+		$avg_score = ($avg_score * $nb_games + points).to_f / ($nb_games + 1)
+		$nb_games += 1
+		$length_record = points if points > $length_record
+
+	else
+		@message = "This is not a valid answer, try again!"
+	end
+
+	@game = Game.new
+	@game.time_start = Time.now
+	session[:new_entry] = @game
+
   erb :index
 end
--- 03. Numbers-and-letters/app/models/game.rb
+++ 03. Numbers-and-letters/app/models/game.rb
@@ -7,18 +7,47 @@ require 'json'
 # Note : you could have used class variables instead.
 $nb_games = 0
 $avg_score = 0
+$length_record = 0
+WORD_LENGTH = 9
 
 class Game
   
-  # Which accessors do you need ?
+	attr_accessor :answer, :time_start
+	attr_reader :grid
   
   def initialize
-    # Which instance variables do initialize on a new object ?
+    random_grid(WORD_LENGTH)
   end
   
-  # What is the public interface ?
+  def points
+  	self.translate_answer == 'nothing!' ? 0 : @answer.length
+  end  
     
+	def test_answer?
+		begin
+			test = @grid.dup
+			@answer.upcase.each_char { |letter| test.delete_at(test.index(letter)) }
+			@grid.length == test.length + @answer.length
+		rescue
+			return false
+		end
+	end
+
+	def translate_answer
+		begin
+			h = JSON.parse(open("http://api.wordreference.com/affd4/json/enfr/" + @answer).read)
+			h["term0"]["PrincipalTranslations"]["0"]["FirstTranslation"]["term"]
+		rescue
+			return 'nothing!'
+		end
+	end
+
   private
-  # Methods that should be kept private
-    
+
+	def random_grid(n)
+		result = []
+		n.times { result << ('A'..'Z').to_a[rand(26)]}
+		@grid = result
+	end
+
 end
