3ad69298bffbf645d5dd8fb67c730ea19c159e3c
Olivier Xu <olivier.xu@gmail.com>
2014-02-06 14:54:15 +0100

    third attempt at jukebox

--- 04. Jukebox/app/controllers/index.rb
+++ 04. Jukebox/app/controllers/index.rb
@@ -3,6 +3,8 @@ get '/' do
 end
 
 get '/new' do
+  @album = Album.new
+  @post_url = "/"
   erb :form
 end
 
@@ -12,6 +14,22 @@ post '/' do
 end
 
 get '/:id' do 
-  @album = Album.find(params[:index].to_i)
+  @album = Album.find(params[:id].to_i)
   erb :show
+end
+
+get '/:id/edit' do 
+  @album = Album.find(params[:id].to_i)
+  @post_url = "/#{params[:id]}"
+  erb :form
+end
+
+post '/:id' do
+	Album.find(params[:id].to_i).update(params[:album])
+  erb :index
+end
+
+get '/:id/delete' do 
+  Album.find(params[:id].to_i).delete
+  erb :index
 end
